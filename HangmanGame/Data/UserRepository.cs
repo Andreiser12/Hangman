04ca9a0c9ac278134060b1c4b4c1b4cc2d2018d7
Andrei Serbanescu <andreiserbanescu35@gmail.com>
2026-04-14 12:40:03 +0300

    - Add UserRepository class for loading and saving users to JSON file.

---
+++ HangmanGame/Data/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using HangmanGame.Models;
+using System.IO;
+using System.Text.Json;
+
+namespace HangmanGame.Data
+{
+    public class UserRepository
+    {
+        private readonly string _filePath;
+
+        public UserRepository()
+        {
+            _filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "users.json");
+        }
+
+        public List<User> LoadUsers()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<User>();
+            }
+            var json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
+        }
+
+        public void SaveUsers(List<User> users)
+        {
+            string directory = Path.GetDirectoryName(_filePath);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            var json = JsonSerializer.Serialize(users, new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_filePath, json);
+        }
+    }
+}
